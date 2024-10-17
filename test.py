b405e1d9d9ad223cb5986e8cb2963d0524fd4f9f
кванториум <aasas@mail.ru>
2024-10-17 19:57:58 +0300

    hty

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-from config import API_KEY
-
-url = "https://api.air.fail/public/text/"
-headers = {"Authorization": API_KEY}
-data = {"email": "sergomet.225@gmail.com", "password": "Programmist5742"}
-response = requests.get(url, headers=headers)
-print(response.json())
