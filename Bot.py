eeb04dac65d963c59cdfdd314e67f5301437a197
кванториум <aasas@mail.ru>
2024-10-31 19:53:53 +0300

    31.10

--- Bot.py
+++ Bot.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 class ModelEnum(Enum):
     gpt_text = 1
-    gpt_image =2
+    gpt_image = 2
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -55,8 +55,8 @@ async def profile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         f"Подписка: {subscription_type}\n\n"
         f"Лимиты: {tokens} token"
     )
-    async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-     help_text = (
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    help_text = (
         "Чтобы начать, пиши /start \n"
         "Чтобы пополнить токены /store \n"
     )
@@ -86,29 +86,37 @@ async def process_message (update: Update, context: ContextTypes.DEFAULT_TYPE) -
      mess = "Пополните баланс в store"
      await update.message.reply_text(mess)
     pandora.close()
+
 async def store(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     user_id = str(user.id)
-    await update.message.reply_text("Добро пожаловать в магазин! Сколько токенов хочешь купить?")
+    await update.message.reply_text("Добро пожаловать в магазин!Ты приобрёл 20 токенов")
     pandora = shelve.open('pandora')
-    pandora[user_id]['tokens'] = 20
-    pandora[user_id]['subs'] = 'VIP'
-async def mode(update: Update, context: CallbackContext) -> None:
+    user_data ={
+     "user_name" : user.full_name,
+     "subs" : "VIP",
+     "tokens" : int(pandora[user_id]["tokens"]) + 20,
+     "model" : ModelEnum.gpt_image.value
+          }
+    pandora[user_id] = user_data
+    pandora.close()
+
+async def mode(update: Update, context: CallbackContext):
     keyboard = [
-      [InlineKeyboardButton("GPT 3.5", callback_data="1")],
-      [InlineKeyboardButton("Stable Diffusion", callback_data="2")]
+        [InlineKeyboardButton("GPT 3.5", callback_data="1")],
+        [InlineKeyboardButton("Stable Diffusion", callback_data="2")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await  update.message.reply_text("Тут вы можете сменить модель нейросетей",reply_markup = reply_markup)
+    await update.message.reply_text("Тут вы можете сменить модель нейросетей", reply_markup=reply_markup)
 
 async  def button(update: Update, context: CallbackContext):
     pandora = shelve.open("pandora")
     query = update.callback_query
     mode_gpt = query.data
-    user_model =pandora[user_id_chat]
+    user_id_chat = str(query.from_user.id)
+    user_model = pandora[user_id_chat]
     user_model["model"] = int(mode_gpt)
     pandora[user_id_chat] = user_model
-
     pandora.close()
 
     new_keyboard = [
@@ -119,13 +127,15 @@ async  def button(update: Update, context: CallbackContext):
     for row in new_keyboard:
         for button in row:
             if button.callback_data == mode_gpt:
-                new_button =InlineKeyboardButton(f"✅{button.text}",callback_data = button.callback_data)
+                new_button = InlineKeyboardButton(f"✅ {button.text}", callback_data=button.callback_data)
                 row[row.index(button)] = new_button
-    reply_markup =InlineKeyboardMarkup(new_keyboard)
+
+    reply_markup = InlineKeyboardMarkup(new_keyboard)
     await query.edit_message_text(
-        text ="Тут вы можете сменить модель нейросетей",
-        reply_markup = reply_markup
+        text="Тут вы можете сменить модель нейросетей",
+        reply_markup=reply_markup
     )
+
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
@@ -133,10 +143,11 @@ def main() -> None:
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("profile", profile))
     application.add_handler(CommandHandler("store", store))
     application.add_handler(CommandHandler("mode", mode))
+    application.add_handler(CallbackQueryHandler(button))
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, process_message))
 
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_KEY = "sk-crEMwJjE69CajbzroTA4IivjVy5KU"
 BOT_KEY = "7014697331:AAGu24mW78fWbSofq4kZo9-8VgfqhbwSmm4"
-API_KEY_IMAGE ="sk-SC10u7qDrcg4cR4LnDQx_uPjAWDqq"
+API_KEY_IMAGE ="sk-crEMwJjE69CajbzroTA4IivjVy5KU"
